181be910fd13beffaf5e423ee81343e9d8ef18a8
haidy <haidy.duenas@gmail.com>
2017-08-10 10:28:46 -0700

    Avance

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,30 @@
-var tareas = new Array()
+document.getElementById("add").addEventListener("click", botones);
+ 
+ function botones(){
+ 	var divMayor = document.getElementById('cajas');
+ 	var myDiv = document.createElement('div');
+		myDiv.setAttribute("class","btnDiv");
+	var myBtn = document.createElement("button");
+ 		myBtn.setAttribute("class","myBtn");
+ 		myBtn.setAttribute('onclick', 'insertar()');
+ 		myBtn.innerHTML='Añadir';
+ 	var myBtn2 = document.createElement("button");
+ 		myBtn2.setAttribute("class","myBtn2");
+ 		myBtn2.innerHTML='X';
+ 		myDiv.appendChild(myBtn);
+ 		myDiv.appendChild(myBtn2);
+ 		divMayor.appendChild(myDiv);
+   }
 
-function add() {
-  var etiquetali = document.createElement("li");  //Creamos una etiqueta li
-  var nuevaTarea = document.getElementById("newTarea").value; 
-  var contenidoEtiqueta = document.createTextNode(nuevaTarea); 
-  etiquetali.appendChild(contenidoEtiqueta);
-  document.getElementById("list").appendChild(etiquetali);
-  document.getElementById("newTarea").value = "";
-  var pushArray = {
-  title: nuevaTarea,
-  completed :false,
-  }
-  listaTareas.push(pushArray);
-  }
+ function insertar(){
+ 	var section = document.getElementById('tareas');
+ 	var contenido = document.getElementById('add').value;
+ 	var newDiv = document.createElement('div');
+ 	newDiv.setAttribute('class','divTareas');
+ 	var parrafo = document.createElement('p');
+ 	var texto =document.createTextNode(contenido);
+ 	parrafo.appendChild(texto);
+ 	newDiv.appendChild(parrafo);
+ 	section.appendChild(newDiv);
+ }
+
